e10076a7b2a825edf62da70735d4fa1d7aade0fb
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-07-17 12:29:00 +0100

    deployment fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@
 
 export function Footer() {
     return <section className="flex h-fit bg-gray-200">
-        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, deserunt accusantium ratione, itaque perferendis maiores quo ipsam sunt consectetur nesciunt adipisci excepturi repellat et inventore recusandae cupiditate nihil? Excepturi, quibusdam!</p>
     </section>
 }
 
